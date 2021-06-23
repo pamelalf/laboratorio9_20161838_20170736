19fbc8b3569ea7798e5f4dac1c013caf5bf33ba6
Pamela Luque <pamela.luque@pucp.edu.pe>
2021-06-22 21:50:20 -0500

    Commit

--- src/main/java/com/example/demo/AreaController/AreaWS.java
+++ src/main/java/com/example/demo/AreaController/AreaWS.java
@@ -1,5 +1,6 @@
 package com.example.demo.AreaController;
 
+import com.example.demo.entity.Area;
 import com.example.demo.repository.AreaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Optional;
 
 @Controller
 public class AreaWS {
@@ -23,10 +27,26 @@ public class AreaWS {
     }
 
     @GetMapping(value="/area/{id}",produces= MediaType.APPLICATION_JSON_VALUE)
-
     public ResponseEntity obtenerAreaxId(@RequestParam("id") Integer id){
+        HashMap<String, Object> responseMap = new HashMap<>();
 
-        return new ResponseEntity(areaRepository.findById(id), HttpStatus.OK);
+        try {
+            //int id = Integer.parseInt(idStr);
+            Optional<Area> opt = areaRepository.findById(id);
+            if (opt.isPresent()) {
+                responseMap.put("estado", "ok");
+                responseMap.put("area", opt.get());
+                return new ResponseEntity(responseMap, HttpStatus.OK);
+            } else {
+                responseMap.put("estado", "error");
+                responseMap.put("msg", "no se encontró el area con id: " + id);
+                return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+            }
+        } catch (NumberFormatException ex) {
+            responseMap.put("estado", "error");
+            responseMap.put("msg", "El ID debe ser un número");
+            return new ResponseEntity(responseMap, HttpStatus.BAD_REQUEST);
+        }
 
     }
 
