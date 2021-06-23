00b21d5c514b02b76f2e99f516dc73c96477a397
Melanie <melanie.espinoza@pucp.edu.pe>
2021-06-22 23:54:26 -0500

    pr

--- src/main/java/com/example/demo/repository/ProyectoRepository.java
+++ src/main/java/com/example/demo/repository/ProyectoRepository.java
@@ -4,9 +4,7 @@ import com.example.demo.entity.Proyecto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
-import java.util.List;
-import java.util.Optional;
+
 
 @Repository
 public interface ProyectoRepository extends JpaRepository<Proyecto,Integer> {
