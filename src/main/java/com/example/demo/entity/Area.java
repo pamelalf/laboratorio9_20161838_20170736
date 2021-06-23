636cfdb34663d3d24608de4e64f3066f0d6f7557
Pamela Luque <pamela.luque@pucp.edu.pe>
2021-06-22 22:30:02 -0500

    Commit

--- src/main/java/com/example/demo/entity/Area.java
+++ src/main/java/com/example/demo/entity/Area.java
@@ -10,7 +10,22 @@ public class Area {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idarea;
-    public String nombreArea;
+    private String nombrearea;
 
 
+    public int getIdarea() {
+        return idarea;
+    }
+
+    public void setIdarea(int idarea) {
+        this.idarea = idarea;
+    }
+
+    public String getNombrearea() {
+        return nombrearea;
+    }
+
+    public void setNombrearea(String nombrearea) {
+        this.nombrearea = nombrearea;
+    }
 }
